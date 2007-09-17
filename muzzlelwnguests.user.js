cd998ade8ffb20d25b4984640dd2dae656846231
Jason Smith <jhs@proven-corporation.com>
2007-09-17 15:54:54 +0700

    Newer port from September 13 or so

--- muzzlelwnguests.user.js
+++ muzzlelwnguests.user.js
@@ -10,23 +10,23 @@
 
 // ==UserScript==
 // @name          muzzleLWNGuests
+// @namespace     http://proven-corporation.com/
 // @description   Make guest postings on lwn.net hidden, but with a button to expose them if desired.
 // @include       http://lwn.net/Articles/*
-// @include       http://www.lwn.net/Articles/*
 // ==/UserScript==
 
 var drabHeadingColor = 'lightgrey';
 
 /* Make a LWN comment's subject heading drab and unattractive.  This function requires being passed the
- * <td> with the actual comment text inside, and it will find the heading itself.  (This will be
+ * DIV with the actual comment text inside, and it will find the heading itself.  (This will be
  * convenient for the code below.)
  */
 function makeDrab(node) {
-    node.parentNode.parentNode.parentNode.parentNode.childNodes[1].style.background = drabHeadingColor;
+    node.parentNode.childNodes[1].style.background = drabHeadingColor;
 }
 
 function makeNormal(node) {
-    node.parentNode.parentNode.parentNode.parentNode.childNodes[1].style.background = null;
+    node.parentNode.childNodes[1].style.background = null;
 }
 
 /* Handle the clicking of the "View" button in guest comments (or "Hide" button if it was
@@ -36,70 +36,60 @@ function makeNormal(node) {
  * attractive/unattractive as appropriate.
  */
 var handleClick = function(event) {
-    var commentMetadata = this.parentNode;
-    var commentBody     = commentMetadata.parentNode.nextSibling;
+    var commentHeading = this.parentNode;
+    var commentBody    = commentHeading.nextSibling;
+    var post           = commentHeading.parentNode;
 
     /* Either display or un-display the comment. */
     if(this.value == 'View') {
         this.value = 'Hide';
 
         /* Make the subject heading and comment text normal. */
-        makeNormal(commentMetadata);
+        makeNormal(post);
         commentBody.style.display = null;
     }
     else if(this.value == 'Hide') {
         this.value = 'View';
 
         /* Make the subject heading unattractive and hide the comment text. */
-        makeDrab(commentMetadata);
+        makeDrab(post);
         commentBody.style.display = 'none';
     }
 };
 
 /* Loop through all comments, find the ones posted by guests. */
-var comments = document.evaluate("//td[@class='CommentBody']", document, null,
+var comments = document.evaluate("//div[@class='CommentBody']", document, null,
                                  XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
 var guestFinder = /(Posted .* by guest $)/;
 for(var a = 0; a < comments.snapshotLength; a++) {
-    var node = comments.snapshotItem(a);
+    var post = comments.snapshotItem(a);
 
-    var commentHeading = node.childNodes[0];
-    var result = guestFinder.exec(commentHeading.nodeValue);
+    var commentHeading = post.childNodes[1];
+    var result = guestFinder.exec(commentHeading.childNodes[0].nodeValue);
     if(result) {
-        makeDrab(node);
+        makeDrab(post);
 
-        /* XXX Kludge XXX:
-         * Convert this comment table into to two rows.  The top row has the permanent "Posted by so-and-so" info,
-         * and the bottom row has the actual post.  This causes a small bug as there is more margin between
-         * the two lines than in normal comments, but it is not noticeable in normal usage, and separating the
-         * comment into two rows makes it easy to hide one of them.
+        /* Put the remainder of the comment (under the "Posted by so-and-so" part) into its own DIV
+         * so that it can be handled all at once.
          */
-        var topRow         = node.parentNode;
-        var commentTable   = topRow.parentNode;
-        var bottomRow      = document.createElement('tr');
-        var bottomTD       = document.createElement('td');
-        bottomTD.className = 'CommentBody';
-        bottomRow.appendChild(bottomTD);
+        var postBody = document.createElement('div');
 
-        /* AFAIK, it is nessary to make an array of child nodes since the childNode attribute is not really
-         * a list.  Modifying child nodes while iterating through childNodes is supposed to be buggy.
-         * Also, start from 5 because 0-4 is the "Posted by so-and-so" stuff that should stay put.
+        /* Move the stuff under the "Posted by..." paragraph inside the div.  I heard a rumor that modifying
+         * childNodes while iterating through it is bad, so do this the hard way.
          */
         var nodesToMove = [];
-        for(var i = 5; i < node.childNodes.length; i++) {
-            nodesToMove.push(node.childNodes[i]);
+        for(var i = 2; i < post.childNodes.length; i++) {
+            nodesToMove.push(post.childNodes[i]);
         }
 
-        /* Perform the actual movement from the original top (and only) row to the bottom row. */
+        /* Perform the relocation. */
         for(var i = 0; i < nodesToMove.length; i++) {
-            node.removeChild(nodesToMove[i]);
-            bottomTD.appendChild(nodesToMove[i]);
+            post.removeChild(nodesToMove[i]);
+            postBody.appendChild(nodesToMove[i]);
         }
 
-        /* Finally, hide the bottom row with the guest's comment. */
-        bottomRow.style.display = 'none';
-
-        commentTable.appendChild(bottomRow);
+        postBody.style.display = 'none';
+        post.appendChild(postBody);
 
         /* Make the button. */
         var displayButton   = document.createElement('input');
@@ -107,6 +97,6 @@ for(var a = 0; a < comments.snapshotLength; a++) {
         displayButton.value = 'View';
         displayButton.addEventListener('click', handleClick, false);
 
-        node.appendChild(displayButton);
+        commentHeading.appendChild(displayButton);
     }
 }
