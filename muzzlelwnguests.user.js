5f39d775e62387054041177a6768be0228bd0919
Jason Smith <jhs@proven-corporation.com>
2007-09-17 15:55:58 +0700

    Some "cleanups" and work to make all comments expandable

--- muzzlelwnguests.user.js
+++ muzzlelwnguests.user.js
@@ -15,18 +15,31 @@
 // @include       http://lwn.net/Articles/*
 // ==/UserScript==
 
-var drabHeadingColor = 'lightgrey';
+var drabCommentColor = 'lightgrey';
+//var commentBoxColor  = null;            /* The normal comment box color; will be set once the page loads. */
 
-/* Make a LWN comment's subject heading drab and unattractive.  This function requires being passed the
- * DIV with the actual comment text inside, and it will find the heading itself.  (This will be
- * convenient for the code below.)
- */
-function makeDrab(node) {
-    node.parentNode.childNodes[1].style.background = drabHeadingColor;
+/* These functions are sort of hard-coded and could change if the HTML of the site changes. */
+function getCommentTitle(comment) {
+    return comment.childNodes[1];
 }
 
-function makeNormal(node) {
-    node.parentNode.childNodes[1].style.background = null;
+function getCommentBody(comment) {
+    return comment.childNodes[3];
+}
+
+function getCommentHeading(comment) {
+    var body = getCommentBody(comment);
+    return body.childNodes[1];
+}
+
+function makeDrab(comment) {
+    getCommentTitle(comment).style.background = drabCommentColor;
+    comment.style.borderColor = drabCommentColor;
+}
+
+function makeNormal(comment) {
+    getCommentTitle(comment).style.background = null;
+    comment.style.borderColor = null;
 }
 
 /* Handle the clicking of the "View" button in guest comments (or "Hide" button if it was
@@ -36,67 +49,91 @@ function makeNormal(node) {
  * attractive/unattractive as appropriate.
  */
 var handleClick = function(event) {
-    var commentHeading = this.parentNode;
-    var commentBody    = commentHeading.nextSibling;
-    var post           = commentHeading.parentNode;
+    var comment        = this.parentNode.parentNode.parentNode;
+    var commentBody    = getCommentBody(comment);
+    var commentText    = commentBody.childNodes[2];
 
     /* Either display or un-display the comment. */
     if(this.value == 'View') {
         this.value = 'Hide';
 
         /* Make the subject heading and comment text normal. */
-        makeNormal(post);
-        commentBody.style.display = null;
+        //makeNormal(comment);
+        commentText.style.display = null;
     }
     else if(this.value == 'Hide') {
         this.value = 'View';
 
         /* Make the subject heading unattractive and hide the comment text. */
-        makeDrab(post);
-        commentBody.style.display = 'none';
+        //makeDrab(post);
+        commentText.style.display = 'none';
     }
 };
 
-/* Loop through all comments, find the ones posted by guests. */
-var comments = document.evaluate("//div[@class='CommentBody']", document, null,
+/* Make an LWN comment expandable/collapsable by adding a "Hide" button and putting the comment
+ * text in a DIV so that it can be shown or hidden by the button.
+ */
+function makeDynamic(comment) {
+    var commentBody = getCommentBody(comment);
+
+    /* All comment text goes in its own compartment so it can be hidden later. */
+    var commentText = document.createElement('div');
+
+    /* Move everything under the CommentPoster paragraph (i.e. "Posted by ...") to the compartment. */
+    var nodesToMove = [];
+    for(var a = 2; a < commentBody.childNodes.length; a++)
+        nodesToMove.push(commentBody.childNodes[a]);
+
+    for(var a = 0; a < nodesToMove.length; a++) {
+        commentBody.removeChild(nodesToMove[a]);
+        commentText.appendChild(nodesToMove[a]);
+    }
+
+    commentBody.appendChild(commentText);
+    commentText.style.display = null;
+
+    /* Make the button. */
+    var displayButton   = document.createElement('input');
+    displayButton.type  = 'button';
+    displayButton.value = 'Hide';
+    displayButton.addEventListener('click', handleClick, false);
+
+    var commentHeading = getCommentHeading(comment);
+    commentHeading.appendChild(displayButton);
+
+    return displayButton;
+}
+
+var guestFinder = /(Posted .* by guest )/;
+
+/* Loop through all comments and make them dynamic. */
+var comments = document.evaluate("//div[@class='CommentBox']", document, null,
                                  XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-var guestFinder = /(Posted .* by guest $)/;
 for(var a = 0; a < comments.snapshotLength; a++) {
-    var post = comments.snapshotItem(a);
+    var comment = comments.snapshotItem(a);
 
-    var commentHeading = post.childNodes[1];
-    var result = guestFinder.exec(commentHeading.childNodes[0].nodeValue);
+    /* If the standard comment box color is unknown, then remember it
+     * (default is #FFCC99 or rgb(255, 204, 153) in LWN's standard scheme.
+     */
+    /*
+    if(! commentBoxColor) {
+        var commentStyle = document.defaultView.getComputedStyle(comment, null);
+        commentBoxColor = commentStyle.getPropertyValue('border-left-color');
+    }
+    */
+
+    var hideButton = makeDynamic(comment);
+
+    /* Guest comments get a drab color and collapse. */
+    var commentHeading = getCommentHeading(comment);
+    var result = guestFinder.exec(commentHeading.textContent);
     if(result) {
-        makeDrab(post);
+        makeDrab(comment);
 
-        /* Put the remainder of the comment (under the "Posted by so-and-so" part) into its own DIV
-         * so that it can be handled all at once.
-         */
-        var postBody = document.createElement('div');
-
-        /* Move the stuff under the "Posted by..." paragraph inside the div.  I heard a rumor that modifying
-         * childNodes while iterating through it is bad, so do this the hard way.
-         */
-        var nodesToMove = [];
-        for(var i = 2; i < post.childNodes.length; i++) {
-            nodesToMove.push(post.childNodes[i]);
-        }
-
-        /* Perform the relocation. */
-        for(var i = 0; i < nodesToMove.length; i++) {
-            post.removeChild(nodesToMove[i]);
-            postBody.appendChild(nodesToMove[i]);
-        }
-
-        postBody.style.display = 'none';
-        post.appendChild(postBody);
-
-        /* Make the button. */
-        var displayButton   = document.createElement('input');
-        displayButton.type  = 'button';
-        displayButton.value = 'View';
-        displayButton.addEventListener('click', handleClick, false);
-
-        commentHeading.appendChild(displayButton);
+        /* Click the collapse button. */
+        var e = document.createEvent('MouseEvents');
+        e.initMouseEvent('click', true, true, window,
+            0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        hideButton.dispatchEvent(e);
     }
 }
