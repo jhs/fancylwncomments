ec762ca979f5a7fd8e67fdb73af4a5ece5e63e2b
Jason Smith <jhs@proven-corporation.com>
2008-07-19 14:04:49 +0700

    Tighten up the description a little

--- fancylwncomments.user.js
+++ fancylwncomments.user.js
@@ -13,7 +13,7 @@
 // ==UserScript==
 // @name          fancyLWNComments
 // @namespace     http://proven-corporation.com/
-// @description   Improved comment browsing in lwn.net: ignore guest posts, previously-read posts with color coding.
+// @description   Improved comment UI in lwn.net: ignore guests and previously-read posts, with color coding.
 // @include       http://lwn.net/Articles/*
 // ==/UserScript==
 
